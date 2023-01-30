412ccfce37c7775d92d4062db189c17f0c2b4b4b
hamo-o <yheel369@gmail.com>
2023-01-30 14:30:54 +0900

    Feat: add base api

--- src/apis/axios.tsx
+++ src/apis/axios.tsx
@@ -3,9 +3,8 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: process.env.REACT_APP_WALDREG_BASE_URL,
   headers: {
-    Accept: 'application/json',
+    Accept: 'application/json;charset=utf-8',
     'Api-version': '1.0',
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
 });
--- src/routes/Setting/Character/index.tsx
+++ src/routes/Setting/Character/index.tsx
@@ -1,13 +1,28 @@
 import { api } from '../../../apis/axios';
 
 const Character = () => {
+  const handleClickSignUp = async () => {
+    try {
+      const response = await api.post('/user', {
+        name: '홍길동',
+        user_id: 'ham',
+        user_password: '1234!a5678',
+        phone_number: '010-1234-1234',
+      });
+
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleClickGetToken = async () => {
     try {
       const response = await api.post('/token', {
         user_id: 'ham',
-        user_password: '1234!5678',
+        user_password: '1234!a5678',
       });
-      console.log(response.data);
+      console.log(response.data.access_token);
     } catch (error) {
       console.log(error);
     }
@@ -15,6 +30,7 @@ const Character = () => {
 
   return (
     <div>
+      <button onClick={handleClickSignUp}>회원가입</button>
       <button onClick={handleClickGetToken}>토큰 발급</button>
     </div>
   );
