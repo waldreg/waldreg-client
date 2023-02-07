538fc047f4a560dc80b2c74ca5a8f235742f3f43
parkyejin <uiop5809@naver.com>
2023-02-07 15:11:13 +0900

    Refactor: board queryKey

--- src/hooks/board/usePostList.tsx
+++ src/hooks/board/usePostList.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import { Post } from "../../interfaces/board";
+import { boardKeys } from "../../types/settingKeys";
 import { boardAPI } from "./../../apis/boardAPI";
 
 interface UsePostList {
@@ -7,6 +8,6 @@ interface UsePostList {
 }
 
 export function usePostList(): UsePostList {
-  const { data: posts } = useQuery<Post[]>("posts", boardAPI.getPostList);
+  const { data: posts } = useQuery<Post[]>(boardKeys.all, boardAPI.getPostList);
   return { posts };
 }
