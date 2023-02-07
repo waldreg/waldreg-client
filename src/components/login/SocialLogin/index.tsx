e01f3730cc29796e1bb13d086b6e71c0d246e764
parkyejin <uiop5809@naver.com>
2023-02-07 23:12:31 +0900

    Remove: images 폴더 삭제

--- src/components/login/SocialLogin/index.tsx
+++ src/components/login/SocialLogin/index.tsx
@@ -1,14 +1,8 @@
 import SocialLoginButton from "../SocialLoginButton";
-import socialLogin_google from "../../../assets/images/socialLogin_google.svg";
 
 const SocialLogin = () => {
   return (
     <>
-      <SocialLoginButton
-        imgSRC={socialLogin_google}
-        provider="Google"
-        href=""
-      />
       {/* TODO 네이버 소셜 로그인*/}
 
       {/* TODO 카카오 소셜 로그인*/}
