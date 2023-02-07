aba500e07e57159a8176a745a9a040cd916f69d1
parkyejin <uiop5809@naver.com>
2023-02-07 23:31:51 +0900

    Refactor: Board apis와 custom hook 합치기

--- src/hooks/board/useBoardList.tsx
+++ src/hooks/board/useBoardList.tsx
@@ -1,16 +1,21 @@
 import { useQuery } from "react-query";
 import { Board } from "../../interfaces/board";
 import { boardKeys } from "../../types/settingKeys";
-import { boardAPI } from "../../apis/boardAPI";
+import axios from "axios";
+
+async function getBoardList(): Promise<Board[]> {
+  const { data } = await axios.get(
+    // `/boards?category=${category_id}&from=${from}&to=${to}`
+    "http://localhost:8001/boards"
+  );
+  return data;
+}
 
 interface UseBoardList {
   boardList?: Board[];
 }
 
 export function useBoardList(): UseBoardList {
-  const { data: boardList } = useQuery<Board[]>(
-    boardKeys.all,
-    boardAPI.getBoardList
-  );
+  const { data: boardList } = useQuery<Board[]>(boardKeys.all, getBoardList);
   return { boardList };
 }
