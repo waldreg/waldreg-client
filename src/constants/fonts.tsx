85e42d43d3eb01f15a28cdc95e476e69d877591b
hamo-o <yheel369@gmail.com>
2023-02-07 19:10:30 +0900

    Fix: change css to cssproperties

--- src/constants/fonts.tsx
+++ src/constants/fonts.tsx
@@ -1,45 +1,59 @@
-import { css } from 'styled-components';
+import { CSSProperties } from 'react';
+
+const HEADING: CSSProperties = {
+  fontSize: '1.75rem',
+  fontWeight: 'bold',
+  lineHeight: '150%',
+  letterSpacing: '-0.4px',
+};
+
+const SUBTITLE1: CSSProperties = {
+  fontSize: '1rem',
+  fontWeight: 'semibold',
+  lineHeight: '140%',
+  letterSpacing: '-0.4px',
+};
+
+const SUBTITLE2: CSSProperties = {
+  fontSize: '1.125rem',
+  fontWeight: 'semibold',
+  lineHeight: '140%',
+};
+
+const BODY1: CSSProperties = {
+  fontSize: '1rem',
+  fontWeight: 'medium',
+  lineHeight: '160%',
+  letterSpacing: '-0.4px',
+};
+
+const BODY_2: CSSProperties = {
+  fontSize: '1rem',
+  fontWeight: 'medium',
+  lineHeight: '180%',
+};
+
+const DETAIL1: CSSProperties = {
+  fontSize: '0.75rem',
+  fontWeight: 'bold',
+  lineHeight: '180%',
+};
+
+const NUMBER1: CSSProperties = {
+  fontSize: '4rem',
+  fontWeight: 'bold',
+  lineHeight: '140%',
+  letterSpacing: '-1.2px',
+};
 
 const FONT = {
-  HEADING: css`
-    font-size: 1.75rem;
-    font-weight: bold;
-    line-height: 150%;
-    letter-spacing: -0.4px;
-  `,
-  SUBTITLE1: css`
-    font-size: 1rem;
-    font-weight: semibold;
-    line-height: 140%;
-    letter-spacing: -0.4px;
-  `,
-  SUBTITLE2: css`
-    font-size: 1.125rem;
-    font-weight: semibold;
-    line-height: 140%;
-  `,
-  BODY1: css`
-    font-size: 1rem;
-    font-weight: medium;
-    line-height: 160%;
-    letter-spacing: -0.4px;
-  `,
-  BODY_2: css`
-    font-size: 1rem;
-    font-weight: medium;
-    line-height: 180%;
-  `,
-  DETAIL1: css`
-    font-size: 0.75rem;
-    font-weight: bold;
-    line-height: 180%;
-  `,
-  NUMBER1: css`
-    font-size: 4rem;
-    font-weight: bold;
-    line-height: 140%;
-    letter-spacing: -1.2px;
-  `,
+  HEADING,
+  SUBTITLE1,
+  SUBTITLE2,
+  BODY1,
+  BODY_2,
+  DETAIL1,
+  NUMBER1,
 };
 
 export default FONT;
