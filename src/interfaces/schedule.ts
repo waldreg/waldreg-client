736c0e1373ff530fb427b90d955d94a3975644f0
parkyejin <uiop5809@naver.com>
2023-02-08 23:06:36 +0900

    Feat: Schedule interface, query key 설정

---
+++ src/interfaces/schedule.ts
@@ -0,0 +1,14 @@
+export interface Schedule {
+  schedule_title: string;
+  schedule_content: string;
+  started_at: string;
+  finish_at: string;
+  repeat: {
+    cycle: number;
+    repeat_finish_at: string;
+  };
+}
+
+export interface ScheduleLists extends Schedule {
+  schedules: Schedule[];
+}
