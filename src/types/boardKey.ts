ce1e9fbe236d7e1dd5f60566707e32b082ae482b
parkyejin <uiop5809@naver.com>
2023-02-11 21:23:08 +0900

    Docs: interface, state, querykey 변경

--- src/types/boardKey.ts
+++ src/types/boardKey.ts
@@ -1,8 +1,4 @@
 export const boardKeys = {
-  all: ["posts"] as const,
+  all: ["boards"] as const,
   detail: (id: number) => [...boardKeys.all, id] as const,
-};
-
-export const boardCategoryKeys = {
-  all: ["boardCategories"] as const,
 };
