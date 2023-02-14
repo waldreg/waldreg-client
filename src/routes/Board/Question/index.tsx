cdaf5ecfc230d997fe0e8e3e5054472f2cdf1e1d
parkyejin <uiop5809@naver.com>
2023-02-14 15:51:01 +0900

    Fix: 게시판 setState 오류 해결

--- src/routes/Board/Question/index.tsx
+++ src/routes/Board/Question/index.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import { useSetRecoilState } from "recoil";
 import PageTitle from "../../../components/common/PageTitle";
-import { boardCategoryState } from "../../../states/board";
 import { Container } from "../Announcement/AnnouncementLayout/style";
 import { BoardContainer } from "../Announcement/style";
 
 const Question = () => {
-  const setBoardCategory = useSetRecoilState(boardCategoryState);
-  setBoardCategory(1);
-
   return (
     <Container>
       <PageTitle>질문게시판</PageTitle>
