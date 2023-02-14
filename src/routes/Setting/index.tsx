ba5dd87e6f6563402ac0533bf8021fdd954c67a9
parkyejin <uiop5809@naver.com>
2023-02-14 19:29:47 +0900

    Refactor: 설정 페이지 > 게시판 관리 페이지

--- src/routes/Setting/index.tsx
+++ src/routes/Setting/index.tsx
@@ -1,12 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
-import UserSettingPage from './User';
-import CharacterPage from './Character';
+import { Routes, Route } from "react-router-dom";
+import UserSettingPage from "./User";
+import CharacterPage from "./Character";
+import BoardManagement from "./Board";
 
 const Setting = () => {
   return (
     <Routes>
       <Route path="/user" element={<UserSettingPage />} />
       <Route path="/character" element={<CharacterPage />} />
+      <Route path="/board" element={<BoardManagement />} />
     </Routes>
   );
 };
