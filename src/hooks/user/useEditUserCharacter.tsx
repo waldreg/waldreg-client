0c014283a458c63864a93157dd5d63aff5c80a74
hamo-o <yheel369@gmail.com>
2023-02-14 09:39:47 +0900

    Feat: add useEditUserChacter custom hook

---
+++ src/hooks/user/useEditUserCharacter.tsx
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from 'react-query';
+
+import { waldregAxios } from '../../apis/axios';
+import { userKeys } from '../../types/userKeys';
+
+const editUserCharacter = async ({
+  id,
+  character,
+}: {
+  id: number;
+  character: string;
+}) => {
+  await waldregAxios.put(`/user/character/${id}`, {
+    character: character,
+  });
+};
+
+const useEditUserCharacter = (userId: string) => {
+  const queryClient = useQueryClient();
+  return useMutation(editUserCharacter, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(userKeys.detail(userId));
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+};
+
+export default useEditUserCharacter;
