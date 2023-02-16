0d86869a2cb404bc211cb896c7fed890d83b57b2
parkyejin <uiop5809@naver.com>
2023-02-16 22:02:57 +0900

    Feat: board category 수정 기능

--- src/components/board/BoardCategoryList/index.tsx
+++ src/components/board/BoardCategoryList/index.tsx
@@ -1,23 +1,68 @@
+import { useCallback, useState } from "react";
 import { useBoardCategoryDelete } from "../../../hooks/board/useBoardCategoryDelete";
+import { useBoardCategoryUpdate } from "../../../hooks/board/useBoardCategoryUpdate";
 import { BoardCategoryLists } from "../../../interfaces/board";
+import Modal from "../../common/modal";
 
 interface BoardCategoryListsProps {
   boardCategoryList: BoardCategoryLists;
 }
 
 function BoardCategoryList({ boardCategoryList }: BoardCategoryListsProps) {
+  const [isOpenModal, setOpenModal] = useState(false);
+  const [categoryName, setCategoryName] = useState<string>("");
+  const [categoryId, setCategoryId] = useState<number>(0);
+
   const categoryDelete = useBoardCategoryDelete();
+  const categoryUpdate = useBoardCategoryUpdate(categoryId, categoryName);
+
+  const onClickUpdateModal = useCallback(() => {
+    setOpenModal(!isOpenModal);
+  }, [isOpenModal]);
 
   return (
     <>
       {boardCategoryList.categories.map((category) => (
         <div key={category.category_id}>
           {category.category_name}
+          <button
+            onClick={() => {
+              onClickUpdateModal();
+              setCategoryId(category.category_id!);
+              setCategoryName(category.category_name);
+            }}
+          >
+            수정
+          </button>
           <button onClick={() => categoryDelete.mutate(category.category_id!)}>
             삭제
           </button>
         </div>
       ))}
+
+      <div>
+        {isOpenModal && (
+          <Modal onClickToggleModal={onClickUpdateModal}>
+            게시판 이름 수정
+            <input
+              type="text"
+              value={categoryName}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setCategoryName(e.currentTarget.value)
+              }
+            />
+            <button onClick={() => setOpenModal(false)}>취소</button>
+            <button
+              onClick={() => {
+                categoryUpdate.mutate();
+                setOpenModal(false);
+              }}
+            >
+              확인
+            </button>
+          </Modal>
+        )}
+      </div>
     </>
   );
 }
