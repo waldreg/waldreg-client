c45a38a0a1e113963a92344eecb8f663defc8027
simeunseo <rkdcjfehowl@naver.com>
2023-02-20 10:26:04 +0900

    Design: constant font weight 수정

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -1,56 +1,56 @@
-import { CSSProperties } from 'react';
+import { CSSProperties } from "react";
 
 const HEADING: CSSProperties = {
-  fontFamily: 'Pretendard',
-  fontSize: '1.5rem',
-  fontWeight: '600',
-  lineHeight: '150%',
-  letterSpacing: '-0.4px',
+  fontFamily: "Pretendard",
+  fontSize: "1.5rem",
+  fontWeight: "600",
+  lineHeight: "150%",
+  letterSpacing: "-0.4px",
 };
 
 const SUBTITLE1: CSSProperties = {
-  fontFamily: 'Pretendard',
-  fontSize: '1rem',
-  fontWeight: 'medium',
-  lineHeight: '140%',
-  letterSpacing: '-0.4px',
+  fontFamily: "Pretendard",
+  fontSize: "1rem",
+  fontWeight: "400",
+  lineHeight: "140%",
+  letterSpacing: "-0.4px",
 };
 
 const SUBTITLE2: CSSProperties = {
-  fontFamily: 'Pretendard',
-  fontSize: '1.125rem',
-  fontWeight: 'medium',
-  lineHeight: '140%',
+  fontFamily: "Pretendard",
+  fontSize: "1.125rem",
+  fontWeight: "500",
+  lineHeight: "140%",
 };
 
 const BODY1: CSSProperties = {
-  fontFamily: 'Pretendard',
-  fontSize: '1rem',
-  fontWeight: '400',
-  lineHeight: '160%',
-  letterSpacing: '-0.4px',
+  fontFamily: "Pretendard",
+  fontSize: "1rem",
+  fontWeight: "400",
+  lineHeight: "160%",
+  letterSpacing: "-0.4px",
 };
 
 const BODY_2: CSSProperties = {
-  fontFamily: 'Pretendard',
-  fontSize: '1rem',
-  fontWeight: '400',
-  lineHeight: '180%',
+  fontFamily: "Pretendard",
+  fontSize: "1rem",
+  fontWeight: "400",
+  lineHeight: "180%",
 };
 
 const DETAIL1: CSSProperties = {
-  fontFamily: 'Pretendard',
-  fontSize: '0.75rem',
-  fontWeight: 'bold',
-  lineHeight: '180%',
+  fontFamily: "Pretendard",
+  fontSize: "0.75rem",
+  fontWeight: "600",
+  lineHeight: "180%",
 };
 
 const NUMBER1: CSSProperties = {
-  fontFamily: 'Pretendard',
-  fontSize: '4rem',
-  fontWeight: 'bold',
-  lineHeight: '140%',
-  letterSpacing: '-1.2px',
+  fontFamily: "Pretendard",
+  fontSize: "4rem",
+  fontWeight: "600",
+  lineHeight: "140%",
+  letterSpacing: "-1.2px",
 };
 
 const FONT = {
