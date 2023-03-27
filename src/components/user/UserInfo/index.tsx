93aaa0cf3dd6015463526b2117a09810b97df7fb
hamo-o <yheel369@gmail.com>
2023-03-27 20:15:18 +0900

    Fix: 유저 정보 추가

--- src/components/user/UserInfo/index.tsx
+++ src/components/user/UserInfo/index.tsx
@@ -5,13 +5,21 @@ import COLOR from '../../../constants/color';
 import FONT from '../../../constants/fonts';
 
 const UserInfo = ({ user, size }: { user: User; size: string }) => {
+  console.log(user);
+
   return (
     <Content size={size}>
       <Name>
         <UserName style={FONT.SUBTITLE3}>{user.name}</UserName>
         <UserId style={FONT.SUBTITLE2}>{user.user_id}</UserId>
       </Name>
-      <Tag style={FONT.SUBTITLE1}>{user.character}</Tag>
+      <Tags>
+        <Tag style={FONT.SUBTITLE1}>{user.character}</Tag>
+        {user.phone_number && (
+          <Tag style={FONT.SUBTITLE1}>{user.phone_number}</Tag>
+        )}
+        <Tag style={FONT.SUBTITLE1}>{user.created_at}</Tag>
+      </Tags>
     </Content>
   );
 };
@@ -41,9 +49,14 @@ const UserId = styled.div`
   color: ${COLOR.GRAY3};
 `;
 
+const Tags = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
 const Tag = styled.div`
   width: max-content;
-  padding: 0.25rem 0.5rem;
+  padding: 0.4rem 1rem;
 
   border: 2px solid ${COLOR.GREEN4};
   border-radius: 0.5rem;
