75f41f6358b36e305f27def585ba2bb54abe0d6e
hamo-o <yheel369@gmail.com>
2023-05-18 16:38:13 +0900

    Faet: useUserList infiniteQuery 변경

--- src/hooks/user/useUserList.tsx
+++ src/hooks/user/useUserList.tsx
@@ -1,21 +1,34 @@
-import { useQuery } from 'react-query';
+import { useInfiniteQuery } from 'react-query';
 
 import { waldregAxios } from '../../apis/axios';
-import { Users } from '../../interfaces/user';
 import { userKeys } from '../../types/userKeys';
 
-const getUserList = async (startIdx: number, endIdx: number) => {
-  const response = await waldregAxios.get(
-    `/users?from=${startIdx}&to=${endIdx}`
-  );
-  return response.data;
-};
+const useUserList = () => {
+  const getUserList = async (startIdx: number, endIdx: number) => {
+    const response = await waldregAxios.get(
+      `/users?from=${startIdx}&to=${endIdx}`
+    );
+    return { data: response.data, idx: endIdx };
+  };
 
-const useUserList = (startIdx: number, endIdx: number) => {
-  const { data } = useQuery<Users>(userKeys.page(startIdx), () =>
-    getUserList(startIdx, endIdx)
+  const {
+    data: userList,
+    fetchNextPage,
+    hasNextPage,
+    isFetched,
+  } = useInfiniteQuery(
+    userKeys.search,
+    ({ pageParam = 1 }) => getUserList(pageParam, pageParam + 9),
+    {
+      getNextPageParam: (lastPage) => {
+        return lastPage.data.max_idx > lastPage.idx + 1
+          ? lastPage.idx + 1
+          : undefined;
+      },
+    }
   );
-  return data;
+
+  return { userList, fetchNextPage, hasNextPage, isFetched };
 };
 
 export default useUserList;
