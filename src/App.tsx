ce2a280787db11f0089ef42220cc073acac86981
hamo-o <yheel369@gmail.com>
2023-06-03 03:35:02 +0900

    Feat: react-query 전역상태관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-import AuthContext from "./states/auth-context";
-import Board from "./routes/Board";
-import GlobalStyle from "./styles/GlobalStyle";
-import Home from "./routes/Home";
-import JoiningPool from "./routes/Setting/JoiningPool";
-import Layout from "./components/global/Layout/index";
-import LoginForm from "./components/auth/login/LoginForm";
-import React from "react";
-import { ReactQueryDevtools } from "react-query/devtools";
-import RewardPage from "./routes/Reward";
-import Schedule from "./routes/Schedule";
-import Setting from "./routes/Setting";
-import SignupForm from "./components/auth/signup/SignupForm";
-import { useContext } from "react";
+import AuthContext from './states/auth-context';
+import Board from './routes/Board';
+import GlobalStyle from './styles/GlobalStyle';
+import Home from './routes/Home';
+import JoiningPool from './routes/Setting/JoiningPool';
+import Layout from './components/global/Layout/index';
+import LoginForm from './components/auth/login/LoginForm';
+import React from 'react';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import RewardPage from './routes/Reward';
+import Schedule from './routes/Schedule';
+import Setting from './routes/Setting';
+import SignupForm from './components/auth/signup/SignupForm';
+import { useContext } from 'react';
+import useApiError from './hooks/error/useApiError';
 
 const queryClient = new QueryClient();
 
 function App() {
   const authCtx = useContext(AuthContext);
+  const { handleError } = useApiError();
+
+  console.log(queryClient);
+
+  queryClient.setDefaultOptions({
+    queries: { onError: (error: any) => handleError(error) },
+    mutations: {
+      onError: (error: any) => {
+        console.log('에러 app', error);
+        handleError(error);
+      },
+    },
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalStyle />
