0b1c07d4544e530a696bde79719438b886eff412
parkyejin <uiop5809@naver.com>
2023-06-29 00:05:55 +0900

    Fix: 검색 기능 구현 (#143)

--- src/components/Icons/BoardIcons.tsx
+++ src/components/Icons/BoardIcons.tsx
@@ -253,20 +253,20 @@ export const SearchIcon = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M1 7.14554C0.999557 4.21437 3.06975 1.69108 5.94452 1.11883C8.81929 0.546586 11.698 2.08477 12.82 4.79267C13.9421 7.50057 12.995 10.624 10.558 12.2528C8.12104 13.8815 4.87287 13.562 2.8 11.4895C1.64763 10.3376 1.00014 8.77495 1 7.14554Z"
         stroke="white"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M11.489 11.4907L15 15.0017"
         stroke="white"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
--- src/routes/Board/BoardSearch/index.tsx
+++ src/routes/Board/BoardSearch/index.tsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import Container from "../../../components/common/container";
 import FONT from "../../../constants/fonts";
 import COLOR from "../../../constants/color";
+import { useState } from "react";
+import BoardList from "../../../components/board/BoardList";
+import { waldregAxios as axios } from "../../../apis/axios";
 
 interface Option {
   value: string;
@@ -14,34 +17,70 @@ const OPTIONS: Option[] = [
   { value: "author", name: "글쓴이" },
 ];
 
-const handleSearchClick = () => {
-  console.log("검색 버튼 클릭");
-};
-
 const BoardSearch = () => {
+  const [keyword, setKeyword] = useState("");
+  const [type, setType] = useState("title");
+  const [searchOpen, setSearchOpen] = useState(false);
+  const [boardList, setBoardList] = useState(undefined);
+  const [board, setBoard] = useState({ boards: [], max_idx: 0 });
+
+  const handleSearchClick = async () => {
+    setSearchOpen(true);
+    const { data } = await axios.get(
+      `/board/search?type=${type}&keyword=${keyword}&from=${1}&to=${999999}`
+    );
+    setBoardList(data);
+    setBoard(data);
+  };
+
   return (
     <SearchBox>
       <SearchTitle style={FONT.HEADING}>게시글 검색</SearchTitle>
       <SearchBarBox>
-        <SearchSelectBox options={OPTIONS} />
-        <SearchInput style={FONT.SUBTITLE2}></SearchInput>
+        <SearchSelectBox options={OPTIONS} setType={setType} />
+        <SearchInput
+          style={FONT.SUBTITLE2}
+          onChange={(e) => setKeyword(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              handleSearchClick();
+            }
+          }}
+        ></SearchInput>
         <SerachButton onClick={handleSearchClick}>검색</SerachButton>
       </SearchBarBox>
-      <Container
-        height={"default"}
-        style={{
-          margin: "3rem 0 1rem 0",
-          padding: "1rem 2rem",
-          justifyContent: "flex-start",
-        }}
-      ></Container>
+
+      {searchOpen && (
+        <Container
+          height={"default"}
+          style={{
+            margin: "3rem 0 1rem 0",
+            justifyContent: "space-around",
+            borderBottom: "none",
+          }}
+        >
+          {boardList && board.boards.length > 0 ? (
+            <BoardList boardList={boardList} />
+          ) : (
+            <div style={FONT.BODY1}>검색 결과가 없습니다.</div>
+          )}
+        </Container>
+      )}
     </SearchBox>
   );
 };
 
-const SearchSelectBox = (props: { options: Option[] }) => {
+const SearchSelectBox = (props: {
+  options: Option[];
+  setType: (value: string) => void;
+}) => {
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    props.setType(value);
+  };
+
   return (
-    <SelectBox>
+    <SelectBox onChange={handleSelectChange}>
       {props.options.map((option: Option) => (
         <option key={option.value} value={option.value}>
           {option.name}
